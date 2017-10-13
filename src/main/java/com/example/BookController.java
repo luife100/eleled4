5756e102cf3938d645e748f2208b0013c15a7504
luife100 <32078506+luife100@users.noreply.github.com>
2017-10-12 20:52:26 -0500

    Add files via upload

---
+++ src/main/java/com/example/BookController.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+  private BookRepository repository;
+
+  @Autowired
+  public BookController(BookRepository repository) {
+    this.repository = repository;
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public ResponseEntity<Book> get(@PathVariable("id") Long id) {
+    Book book = repository.findOne(id);
+    if (null == book) {
+      return new ResponseEntity<Book>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<Book>(book, HttpStatus.OK);
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public String delete(@PathVariable("id") Long id) {
+       if (id<=0) {
+      return "Error";
+    }
+            repository.delete(id);
+    return "Deleted id:"+id;
+  }
+
+  @RequestMapping(value = "/new", method = RequestMethod.POST)
+  public ResponseEntity<Book> update(@RequestBody Book book) {
+    repository.save(book);
+    return get(book.getId());
+  }
+
+  @RequestMapping
+  public List<Book> all() {
+    return repository.findAll();
+  }
+  //to delete:
+
+}
